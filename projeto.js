28ee6c437047c38ed1d6098cc9da11c0c9034a2c
Paulo-Victork <paulovictor2017k@gmail.com>
2023-12-08 13:06:58 -0300

    meu primeiro commit

---
+++ projeto.js
@@ -0,0 +1,34 @@
+const nome = ("Paulo")
+let xp = 2099
+const nivel = ["Ferro","Bronze","Prata","Ouro","Platina","Ascendente","Imortal","Radiante"]
+
+
+if (xp>=10001){
+    console.log ("O Herói de nome " + nome + " Está no nível " + nivel[7])
+}
+else if (xp>=9001){
+        console.log ("O Herói de nome " + nome + " Está no nível " + nivel[6])
+    }
+else if (xp>=8001){
+    console.log ("O Herói de nome " + nome + " Está no nível " + nivel[5])
+}
+else if (xp>=7001){
+    console.log ("O Herói de nome " + nome + " Está no nível " + nivel[4])
+}
+else if (xp>=6001){
+    console.log ("O Herói de nome " + nome + " Está no nível " + nivel[3])
+}
+else if (xp>=2001){
+    console.log ("O Herói de nome " + nome + " Está no nível " + nivel[2])
+}
+else if (xp>=1001){
+    console.log ("O Herói de nome " + nome + " Está no nível " + nivel[1])
+}
+else if (xp<=1000){
+    console.log ("O Herói de nome " + nome + " Está no nível " + nivel[0])
+}
+
+
+
+
+
